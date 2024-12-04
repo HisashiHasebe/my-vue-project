b402c54b19ae1a07230163f96a8f5e5ae2b9973f
HisashiHasebe <hhasebe@diverta.co.jp>
2024-12-04 13:17:24 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello, Kuroco!</h1>
+    <p>このコンポーネントはVueで作成されました。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
